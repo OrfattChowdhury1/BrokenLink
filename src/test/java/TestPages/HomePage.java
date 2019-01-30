398eb559d9e77b0f8acd655fa050e6fc14f8fbea
OrfattChowdhury1 <orfatt11@gmail.com>
2019-01-29 23:44:04 -0800

    first commit

---
+++ src/test/java/TestPages/HomePage.java
@@ -0,0 +1,14 @@
+package TestPages;
+
+import base.CommonAPI;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class HomePage extends CommonAPI {
+    @Test
+    public void findBrokenLinks() throws IOException {
+        brokenLink();
+    }
+
+}
